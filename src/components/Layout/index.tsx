a1f055f6fd51d14d0e4b26aacaf275ada79f6bb0
ardodeardo <simadanaga@gmail.com>
2023-04-11 21:56:22 +0700

    chore: gfont crossOrigin attribute

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -32,7 +32,7 @@ function Layout(props: layout) {
 
         {/* google font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet" />
       </Head>
 
